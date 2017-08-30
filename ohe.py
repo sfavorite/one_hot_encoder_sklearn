c5088362256a88d8b4f4172c5b618faa5bb36339
Scot Favorite <scot.favorite@gmail.com>
2017-08-30 17:01:33 -0400

    Add OneHotEncoder

--- ohe.py
+++ ohe.py
@@ -1,18 +1,23 @@
 #!/usr/bin/env python
 from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import OneHotEncoder
+import numpy as np
 
 # Categorical data
-data = ["red", "blue", "green", "black"]
+data = np.array(["red", "blue", "green", "black"])
+print(data)
 
 # Set up a label encoder
 le = LabelEncoder()
 
 # Fit the data
-le.fit(data)
-
-print(le.classes_)
-
-transformed_cat = le.transform(data)
+transformed_cat = le.fit_transform(data)
 print(transformed_cat)
 
-le.inverse_transform(transformed_cat)
+
+# Initialize an Encoder and fit the data
+enc = OneHotEncoder()
+enc.fit_transform(transformed_cat.reshape(-1, 1)).toarray()
+
+# Print the new values
+print(enc.feature_indices_)
